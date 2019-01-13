18b6621b1204feefed776835707301948772df45
Jacob Morris <blendingjake@gmail.com>
2019-01-13 12:47:46 -0500

    remove unneeded files

--- jv_properties.py
+++ jv_properties.py
@@ -11,7 +11,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from math import radians
-
 from bpy.types import PropertyGroup, Object
 from bpy.props import PointerProperty, EnumProperty, FloatProperty, BoolProperty, IntProperty, FloatVectorProperty, \
     CollectionProperty, StringProperty
